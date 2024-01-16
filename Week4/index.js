fc9718f37cc5700dc231bcd82c93b847704369df
Yash_Yadav <raoyash2020@gmail.com>
2024-01-16 20:18:57 +0530

    JS DOM-Todo

--- Week4/index.js
+++ Week4/index.js
@@ -11,6 +11,8 @@ let time = document.querySelector('#time');
 let InterestButton = document.querySelector('#InterestButton');
 let InterestResult = document.querySelector('#result');
 
+let globalID = 1;
+
 let timeout;
 const debouncedSum = () => {
     // If the user is typing continuosly, this function will be called multiple times in a very short period
@@ -45,4 +47,37 @@ const calculateInterest = async () => {
     const response = await fetch("http://localhost:5000/interest?p="+p+"&r="+r+"&t="+t);
     const ans = await response.text();
     InterestResult.innerHTML = ans
+}
+
+function markAsDone(id)
+{
+    const parent = document.getElementById(id);
+    parent.children[2].innerHTML = "Done";
+}
+
+function createChild(t, d, id)
+{
+    const child = document.createElement("div");
+    const firstGrandChild = document.createElement("div");
+    firstGrandChild.innerHTML = t;
+    const secondGrandChild = document.createElement("div");
+    secondGrandChild.innerHTML = d;
+    const thirdgrandChild = document.createElement("button");
+    thirdgrandChild.innerHTML = "Mark as Done";
+    thirdgrandChild.setAttribute('onclick', `markAsDone(${id})`);
+
+    child.appendChild(firstGrandChild);
+    child.appendChild(secondGrandChild);
+    child.appendChild(thirdgrandChild);
+    child.setAttribute('id',id);
+    return child;
+}
+
+function todo() {
+    let t = document.querySelector("#title").value;
+    let d = document.querySelector('#desc').value;
+
+    let parent = document.querySelector('.container');
+    parent.appendChild(createChild(t, d, globalID++));
+    
 }
